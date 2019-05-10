6869803c71cf7322663363f3385651ca33dfcc8d
Dan Neame <danneame@gmail.com>
2019-05-10 23:36:48 +0000

    Allow init-server.sh to run if tes3mp-server is not currently running

--- init-server.sh
+++ init-server.sh
@@ -3,7 +3,7 @@ set -e
 . bootstrap-config.sh
 
 cd $TES3_DIR
-killall tes3mp-server
+killall -q tes3mp-server || echo "Server was not running"
 echo 0 > nohup.out
 nohup ./tes3mp-server.sh > server.out 2> server.err &
 echo "Server restarted!"
